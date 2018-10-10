e4f013b9ec7b44f499fdf7490cb918e9ce406a84
Nicholas Keuning <nkeuning@umich.edu>
2018-10-10 00:44:10 -0400

    Add voice script (not currently used) and add microphone to project settings

---
+++ MedTest/Assets/Scripts/MainMenuVoiceCommands.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class MainMenuVoiceCommands : MonoBehaviour, ISpeechHandler {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void OnSpeechKeywordRecognized(SpeechEventData eventData)
+    {
+        RespondToVoiceCommand(eventData.RecognizedText);
+    }
+
+    public void RespondToVoiceCommand(string command)
+    {
+        switch (command)
+        {
+            case "Video":
+                break;
+            case "Scans":
+                break;
+            case "Vitals":
+                break;
+            case "Render":
+                break;
+        }
+    }
+}
