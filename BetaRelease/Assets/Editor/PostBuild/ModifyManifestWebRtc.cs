b5a24634d9466077d28b177149e451a7d845b6af
Nicholas Keuning <nkeuning@umich.edu>
2018-11-13 16:34:21 -0500

    Major progress towards beta release

---
+++ BetaRelease/Assets/Editor/PostBuild/ModifyManifestWebRtc.cs
@@ -0,0 +1,45 @@
+﻿using UnityEditor;
+using UnityEditor.Build;
+using UnityEngine;
+//using System.Linq;
+using System.Xml.Linq;
+
+public class ModifyManifestWebRtc : IPostprocessBuild
+{
+    public int callbackOrder { get { return 0; } }
+    public void OnPostprocessBuild(BuildTarget target, string path)
+    {
+        string fileLocation = path + "/ApolloLens/Package.appxmanifest";
+        XDocument configDoc = XDocument.Load(fileLocation);
+        Debug.Log("MyCustomBuildProcessor.OnPostprocessBuild for target " + target + " at path " + path);
+        Debug.Log(configDoc.ToString());
+    }
+}
+
+
+
+
+//static class ConfigData
+//{
+
+//    // Database connection string
+//    public static string getConString()
+//    {
+//        XDocument configDoc = XDocument.Load(fileLocation);
+//        return configDoc.Descendants("connectionStrings").Descendants()
+//            .Select(x => x.Attribute("connectionString"))
+//            .First().Value;
+//    }
+
+
+//    // Wireman auotcad blocks version control
+//    public static bool blockCheck(string blockName, string blockVersion)
+//    {
+//        XDocument confingDoc = XDocument.Load(fileLocation);
+//        var xmlVersion = confingDoc.Descendants("Block")
+//            .Where(x => x.Attribute("name").Value == blockName).First()
+//            .Attribute("version").Value;
+//        return (xmlVersion == blockVersion);
+//    }
+//}
+
---
+++ BetaRelease/Assets/Scripts/ApolloLens/GalleryControl.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+
+#if WINDOWS_UWP
+using Windows.Storage;
+using Windows.Storage.Streams;
+#endif
+
+
+public class GalleryControl : MonoBehaviour {
+
+    public RawImage Image;
+
+    private List<string> SubFolderNames;
+    private string CurrentFolder;
+    private int CurrentIndex;
+
+#if WINDOWS_UWP
+    private Dictionary<string, List<StorageFile>> ImageFileDict = new Dictionary<string, List<StorageFile>>();
+#endif
+
+    // Use this for initialization
+    void Start () {
+#if WINDOWS_UWP
+        Initialize();
+#endif
+    }
+
+    // Update is called once per frame
+    void Update() { }
+
+    public void MoveImage(int Amount)
+    {
+#if WINDOWS_UWP
+        // adjust current index by specified amount
+        int NewIndex = CurrentIndex + Amount;
+
+        // constrain index within bounds
+        int numImages = ImageFileDict[CurrentFolder].Count();
+        NewIndex = (NewIndex < 0) ? 0 : NewIndex;
+        NewIndex = (NewIndex >= numImages) ? numImages - 1 : NewIndex;
+
+        // set the image texture to newly indexed image if needed
+        if (NewIndex != CurrentIndex)
+        {
+            CurrentIndex = NewIndex;
+            SetTexture();
+        }
+#endif
+    }
+
+#if WINDOWS_UWP
+    private async void Initialize()
+    {
+        // get and store all subfolders of Pictures/Surgery
+        StorageFolder SurgeryFolder = await KnownFolders.PicturesLibrary.GetFolderAsync("Surgery");
+        var SubFolders = await SurgeryFolder.GetFoldersAsync();
+        SubFolderNames = SubFolders.Select(f => f.Name).ToList();
+
+        // get and store all files in each subdirectory
+        foreach (StorageFolder folder in SubFolders)
+        {
+            var ImagesFiles = await folder.GetFilesAsync();
+            ImageFileDict[folder.Name] = ImagesFiles.ToList();
+        }
+
+        // set current folder and image index within folder
+        CurrentFolder = SubFolderNames.First();
+        CurrentIndex = 0;
+
+        // set image to current image
+        SetTexture();
+    }
+
+    private async void SetTexture()
+    {
+        // get the storage file corresponding to current image
+        StorageFile ImageFile = ImageFileDict[CurrentFolder][CurrentIndex];
+
+        // get byte array for image
+        byte[] ImageBytes = await GetBytesAsync(ImageFile);
+
+        // create texture2d from byte array
+        Texture2D ImageTexture = new Texture2D(2, 2);
+        ImageTexture.LoadImage(ImageBytes);
+
+        // set image texture to newly created texture
+        Image.texture = ImageTexture;
+    }
+
+    private static async Task<byte[]> GetBytesAsync(StorageFile file)
+    {
+        IBuffer buffer = await FileIO.ReadBufferAsync(file);
+        byte[] bytes = buffer.ToArray();
+        return bytes;
+    }
+#endif
+}
